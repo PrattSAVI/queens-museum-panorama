eb39996d20dfa74b38bba4a18a5a2f4ebe458081
Murray Cox <code@murraycox.com>
2022-10-16 22:25:11 -0400

    initial commit

---
+++ components/panorama-components.js
@@ -0,0 +1,32 @@
+import {LitElement, html, css} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
+    
+class PanoramaNav extends LitElement {
+  
+  static get styles() {
+    return css`
+
+      div {
+        display: inline;
+      }
+
+      a {
+        text-decoration: none; 
+      }
+
+      a, a:visited, a:hover, a:active {
+        color: inherit;
+      }
+      
+
+    `;
+  }
+
+  render() {
+    return html`<div><a href="/">Home</a></div
+                <div>Recorded Text</div>
+                <div>Language</div>`;
+  }
+  
+}
+
+customElements.define('panorama-nav', PanoramaNav);
---
+++ readme.md
@@ -0,0 +1,22 @@
+:
+
+Dependencies,
+
+None, except scripts use lit-element, via an external script
+
+Running
+
+Copy to a webserver, or install a local one:
+
+http-server
+
+install http-server
+
+npm -i -g http-server?
+
+cd to the directory and use:
+
+http-server
+
+Tests:
+??
