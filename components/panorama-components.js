4871a5f174dee9b38b1277a3ad27ffecb12799bb
Murray Cox <code@murraycox.com>
2022-11-07 13:40:04 -0500

    Style navigation, introduce nav for explore section

--- components/panorama-components.js
+++ components/panorama-components.js
@@ -1,32 +1,66 @@
 import {LitElement, html, css} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
-    
+
+const navStyles = css`
+  .nav {
+    margin: 20px 0;
+  }
+
+  .nav div {
+    display: inline;
+    border: 1px solid black;
+    border-radius: 5px;
+    padding: 4px;
+  }
+
+  .nav div:hover {
+    background-color: black;
+    color: white;
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none; 
+  }
+
+  a, a:visited, a:hover, a:active {
+    color: inherit;
+  }`
+
 class PanoramaNav extends LitElement {
   
   static get styles() {
-    return css`
-
-      div {
-        display: inline;
-      }
-
-      a {
-        text-decoration: none; 
-      }
-
-      a, a:visited, a:hover, a:active {
-        color: inherit;
-      }
-      
-
-    `;
+    return navStyles;
   }
 
   render() {
-    return html`<div><a href="./">Home</a></div
-                <div>Recorded Text</div>
-                <div>Language</div>`;
+    return html`
+      <div class="nav">
+        <div><a href="./">Home</a></div>
+        <div>Recorded Text</div>
+        <div>Language</div>
+      </div>`;
+  }
+  
+}
+
+class ExploreNav extends LitElement {
+  
+  static get styles() {
+    return navStyles;
+  }
+
+  render() {
+    return html`
+      <div class="nav">
+        <div><a href="./">Home</a></div>
+        <div>How to use the maps</div>
+        <div>3d Panorama</div>
+        <div>Add your story to the map</div>
+        <div>Back</div>
+      </div>`;          
   }
   
 }
 
 customElements.define('panorama-nav', PanoramaNav);
+customElements.define('explore-nav', ExploreNav);
