6d0c2fe392085ca0f613c6865ac9c69d52187dc4
Murray Cox <code@murraycox.com>
2023-01-02 21:17:22 -0500

    Connect some landmarks to flyTo of the viewer, and added a marker for Queens Museum

--- js/city-landmarks-tour.js
+++ js/city-landmarks-tour.js
@@ -1,21 +1,49 @@
-    // Your access token can be found at: https://cesium.com/ion/tokens.
-    // This is the default access token from your ion account
+let viewer = null
+// Your access token can be found at: https://cesium.com/ion/tokens.
+// This is the default access token from your ion account
 
-    Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlZDAyZDcyNy00NGQwLTRkODItYjYzZS0xODk1ZDEyZGVjZWIiLCJpZCI6MTA3ODg2LCJpYXQiOjE2NjMwODM2MTV9.nT8QVqASAbBjcvuHZyFKIjhB6r1ncT-hFgL8XXkXFnA';
-    // Initialize the Cesium Viewer in the HTML element with the `cesium-container` ID.
-    const viewer = new Cesium.Viewer('cesium-container', {
-      terrainProvider: Cesium.createWorldTerrain()
-    });    
-    // Add Cesium OSM Buildings, a global 3D buildings layer.
-    //const buildingTileset = viewer.scene.primitives.add(Cesium.createOsmBuildings());   
-    // Fly the camera to the Queens Museum at the given longitude, latitude, and height.
-    viewer.camera.flyTo({
-      destination : Cesium.Cartesian3.fromDegrees(-73.846707, 40.7458395, 400),   
-      orientation : {
-        heading : Cesium.Math.toRadians(0.0),
-        pitch : Cesium.Math.toRadians(-15.0),
-      }
-    });
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlZDAyZDcyNy00NGQwLTRkODItYjYzZS0xODk1ZDEyZGVjZWIiLCJpZCI6MTA3ODg2LCJpYXQiOjE2NjMwODM2MTV9.nT8QVqASAbBjcvuHZyFKIjhB6r1ncT-hFgL8XXkXFnA';
+// Initialize the Cesium Viewer in the HTML element with the `cesium-container` ID.
+viewer = new Cesium.Viewer('cesium-container');
+const tileset = viewer.scene.primitives.add(
+  new Cesium.Cesium3DTileset({
+    url: Cesium.IonResource.fromAssetId(1409363),
+  })
+);    
+   
+
+var queensMuseumMarker = viewer.entities.add({
+  name : 'Queens Museum',
+  position : Cesium.Cartesian3.fromDegrees(-73.846707, 40.7458395, 100),
+  point : {
+      pixelSize : 50,
+      color : Cesium.Color.RED,
+      outlineColor : Cesium.Color.WHITE,
+      outlineWidth : 2
+  },
+  label : {
+      text : 'Queens Museum',
+      font : '14pt monospace',
+      style: Cesium.LabelStyle.FILL_AND_OUTLINE,
+      outlineWidth : 2,
+      verticalOrigin : Cesium.VerticalOrigin.BOTTOM,
+      pixelOffset : new Cesium.Cartesian2(0, -30)
+  }
+});
+
+
+// Add Cesium OSM Buildings, a global 3D buildings layer.
+//const buildingTileset = viewer.scene.primitives.add(Cesium.createOsmBuildings());   
+// Fly the camera to the Queens Museum at the given longitude, latitude, and height.
+viewer.camera.flyTo({
+  destination : Cesium.Cartesian3.fromDegrees(-73.846707, 40.7458395, 400),   
+  orientation : {
+    heading : Cesium.Math.toRadians(0.0),
+    pitch : Cesium.Math.toRadians(-15.0),
+  }
+});
+
+
 
 
 
@@ -28,4 +56,24 @@ function onToggleSidePanel(){
     document.getElementById("toggle-side-panel-label").textContent = "Open"
   else
     document.getElementById("toggle-side-panel-label").textContent = "Close"
+}
+
+function onToggleSidePanel(){
+  const sidePanel = document.getElementById("side-panel")
+  sidePanel.classList.toggle("collapsed")
+  // TODO use an icon, not text
+  if (sidePanel.classList.contains("collapsed"))
+    document.getElementById("toggle-side-panel-label").textContent = "Open"
+  else
+    document.getElementById("toggle-side-panel-label").textContent = "Close"
+}
+
+function flyTo(longitude, latitude){
+  viewer.camera.flyTo({
+    destination : Cesium.Cartesian3.fromDegrees(longitude, latitude, 400),   
+    orientation : {
+      heading : Cesium.Math.toRadians(0.0),
+      pitch : Cesium.Math.toRadians(-15.0),
+    }
+  });
 }
--- js/zoom-to-the-panorama.js
+++ js/zoom-to-the-panorama.js
@@ -3,9 +3,14 @@
 
     Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlZDAyZDcyNy00NGQwLTRkODItYjYzZS0xODk1ZDEyZGVjZWIiLCJpZCI6MTA3ODg2LCJpYXQiOjE2NjMwODM2MTV9.nT8QVqASAbBjcvuHZyFKIjhB6r1ncT-hFgL8XXkXFnA';
     // Initialize the Cesium Viewer in the HTML element with the `cesium-container` ID.
-    const viewer = new Cesium.Viewer('cesium-container', {
-      terrainProvider: Cesium.createWorldTerrain()
-    });    
+    const viewer = new Cesium.Viewer('cesium-container');    
+
+    const tileset = viewer.scene.primitives.add(
+      new Cesium.Cesium3DTileset({
+        url: Cesium.IonResource.fromAssetId(1409363),
+      })
+    );  
+
     // Add Cesium OSM Buildings, a global 3D buildings layer.
     //const buildingTileset = viewer.scene.primitives.add(Cesium.createOsmBuildings());   
     // Fly the camera to the Queens Museum at the given longitude, latitude, and height.
@@ -16,16 +21,3 @@
         pitch : Cesium.Math.toRadians(-15.0),
       }
     });
-
-
-
-
-function onToggleSidePanel(){
-  const sidePanel = document.getElementById("side-panel")
-  sidePanel.classList.toggle("collapsed")
-  // TODO use an icon, not text
-  if (sidePanel.classList.contains("collapsed"))
-    document.getElementById("toggle-side-panel-label").textContent = "Open"
-  else
-    document.getElementById("toggle-side-panel-label").textContent = "Close"
-}
