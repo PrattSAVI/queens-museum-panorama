b52bef60073953ad24cc64275a941ed31723cb27
Murray Cox <code@murraycox.com>
2023-01-04 20:01:36 -0500

    Set up tour of flushing meadows corona park

--- js/tour-flushing-meadows-corona-park.js
+++ js/tour-flushing-meadows-corona-park.js
@@ -1,129 +1,107 @@
-const urlid = '1a2f1b172bb44a85986fb018f7f2c6b5'
+//URL ID for the Pratt SAVI Sketchfab Model of the Queens Museum Panorama
+//TODO: Change this to a model in a Queens Museum SketchFab account?
+const urlid = '4bf5a7686e5847efb2b38f69621875ac'
 
 const scrollingStory = document.querySelector('#scrolling-story');
 const essay = document.querySelector('#essay'); // Floating window over the iframe
 const iframe = document.querySelector('#api-frame');
 
-var cameraPosition = null;
-var targetlocation = null;
-var initialCameraPosition = null;
-var initialTouch = null;
-var isDragging = false;
-var cameraSpeed = -0.001;
-var cameraScrollSpeed = 1.5;
-
-// Camera Movement
-function deltapos(x1, x2, step, i) {
-  return x1 + (((x2 - x1) / step) * i)
-}
-
-//Camera Animation
-function updateCamera() {
-  if (viewer.api && viewer.camera && cameraPosition, targetlocation) {
-      viewer.api.setCameraLookAt(cameraPosition, targetlocation, 0);
-  }
-  requestAnimationFrame(updateCamera);
-}
-
-// get IDs for the giver layer. Blend Object give things as Layer:XXX
-function getIDs(result, layer) {
-  let objs = result.children[0].children[0].children[0].children[0].children;
-
-  let ids = [];
-  objs.forEach(el => {
-      if (el.name.includes(layer)) { // Filter Layer
-          ids.push(el.instanceID)
-      }
-  });
-  return ids
-}
-
-// get IDs for the giver layer. Blend Object give things as Layer:XXX
-function getID_Reverse(result, layer) {
-  let objs = result.children[0].children[0].children[0].children[0].children;
-  let ids = [];
-  objs.forEach(el => {
-      if (!el.name.includes(layer)) { // Filter Layer
-          ids.push(el.instanceID)
-      }
-  });
-  return ids
-}
-
 // generic window resize listener event
 function handleResize() {
   // 1. update marginTop of scrolling story so it is just below the fold.
-  scrollingStory.style.marginTop = (window.innerHeight) + "px";
+  //scrollingStory.style.marginTop = (window.innerHeight) + "px";
+  var stepBrowserHeight = d3.selectAll(".step.browser-height")
+
+  // 1. update height of step elements
+  var stepH = Math.floor(window.innerHeight * 1);
+  stepBrowserHeight.style("height", stepH + "px");
+
+}
+
+//Camera Animation
+function updateCamera(cameraPosition, targetLocation) {
+    if (viewer.api && viewer.camera && cameraPosition, targetLocation) {
+        viewer.api.setCameraLookAt(cameraPosition, targetLocation, 2);
+    }
+    //requestAnimationFrame(updateCamera);
+}
+
+function printCameraLookAt(){
+
+    viewer.api.getCameraLookAt(
+        function(err, camera) {
+            console.info(camera);
+        }
+    );
 
 }
 
 
-handleResize();
 // Initializing viewer starts off the API. viewer.api
 // This uses the first iframe
 var viewer = new Viewer(urlid, iframe, function() {
 
-  // Camera Set Up, position and target values
-  let campos = viewer.camera.position
-  cameraPosition = vec3.fromValues(campos[0], campos[1], campos[2])
-  targetlocation = [50828, 9609, 480]; // Look at QM
-  updateCamera(); // Initiate Animate
-
-  // Get Object IDs and integrate into the scroll
-  viewer.api.getSceneGraph(function(err, result) {
-
-      //Get Ids to hide and show
-      let past_prog = 0 // I'll use this to determine direction
-
-      // 1st Scroll interaction is here.
-      var scrollWindow = new ScrollWindow(essay, function(progress) {
-          
-          console.log(progress)
-
-          // Viewer visibility
-          var duration = [-770, -200];
-          var path = [
-              [52115, 10174, 1200],
-              [52115, 10174, 800]
-          ];
-
-          let ids = getIDs(result, "Areas");
-          var objs_count = ids.length
-
-          // ---------------------------------- SHOW / HIDE -------------------------------------------
-          var viewerEl = document.querySelector('#explore');
-
-          // .Viewer contains iframes container, Show and hide the iframe, so fade
-          if (progress > -200 && progress < 100) {
-              if (progress < duration[0] || progress > duration[1]) {
-                  viewerEl.classList.remove('visible'); // Make iframe invisible
-              } else {
-                  viewerEl.classList.add('visible');
-              }
-          }
-
-          // Reset hidden objects
-          if ((progress < 0 || progress > 100) && progress < 120) { // If the iframe has past
-              ids.forEach(el => { viewer.api.show(el) })
-          }
-
-          // Which way is the site being scrolled
-          if (progress > duration[0] & progress < duration[1]) {
-
-              if (cameraPosition) { // Camera Movement over scroll
-                  var x1 = path[0][0];
-                  var x2 = path[1][0];
-                  var y1 = path[0][1];
-                  var y2 = path[1][1];
-                  step = duration[1]
-                      //console.log(progress, deltapos(x1, x2, step, progress), deltapos(y1, y2, step, progress), deltapos(path[0][2], path[1][2], step, progress))
-                  cameraPosition = vec3.fromValues(deltapos(x1, x2, step, progress), deltapos(y1, y2, step, progress), deltapos(path[0][2], path[1][2], step, progress));
-              }
-
-          };
-
-      });
-
-  });
+//   // Camera Set Up, position and target values
+//   let campos = viewer.camera.position
+//   const cameraPosition = vec3.fromValues(campos[0], campos[1], campos[2])
+//   const targetLocation = [50828, 9609, 480]; // Look at QM
 
 });
+
+// scrollama event handlers
+function onStepEnter(response) {
+    console.log(response);
+    // response = { element, direction, index }
+    if (response.element.classList.contains("show-story")){
+        console.log("show-story")
+        scrollingStory.classList.remove("invisible")
+    } else {
+        console.log("hide-story")
+        scrollingStory.classList.add("invisible")
+    }
+
+    switch(response.element.id) {
+        case "introduction":
+            var position = [
+                4.8127636935173115,
+                0.6855157632821853,
+                2.3813691577377445
+            ]
+            var target = [
+                4.785263562323396,
+                0.6476057335162367,
+                -0.17772033105607
+            ]
+            updateCamera(position, target); 
+            break
+        case "scene-2":
+            var position = [
+                4.151882071706647,
+                0.6811372498971952,
+                -0.02787594569011345
+            ]
+            var target = [
+                4.837988964850769,
+                0.7982169126259439,
+                -0.10682129772351641
+            ]
+            updateCamera(position, target); 
+            break
+
+        default:
+    }
+
+}
+
+handleResize();
+
+// initialize the scrollama
+var scroller = scrollama();
+
+scroller
+    .setup({
+        step: "#scrolling-story #story .step",
+        offset: 1,
+        debug: false
+    })
+    .onStepEnter(onStepEnter);
